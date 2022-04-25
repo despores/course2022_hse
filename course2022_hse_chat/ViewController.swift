da5d831c79fc282cc27054b2e57faa59b7182f97
Ernest Sakhabutdinov <ernest.sakhabutdinov@mail.ru>
2022-04-25 20:33:02 +0300

    Initial Commit

---
+++ course2022_hse_chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  course2022_hse_chat
+//
+//  Created by Эрнест Сахабутдинов on 25.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
